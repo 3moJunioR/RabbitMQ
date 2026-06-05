a3afd142e9be2ec9a457fe4e505af2fb2d6192c2
3moJunioR <m.wa7ba@gmail.com>
2026-06-06 02:29:05 +0300

    FanoutExchange

---
+++ FanoutExchange/Consumerr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using var connection=await factory.CreateConnectionAsync();
+using var channel=await connection.CreateChannelAsync();
+
+Console.WriteLine("waiting for message");
+
+await  ConsumeMessage("q.fanout1");
+await  ConsumeMessage("q.fanout2");
+await  ConsumeMessage("q.fanout3");
+await  ConsumeMessage("q.fanout4");
+await  ConsumeMessage("q.fanout5");
+
+Console.ReadLine();
+
+async Task ConsumeMessage(string queue)
+{
+    var consumer = new AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync +=async (sender, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        //Write ur method here , to process the message
+
+        Console.WriteLine($" [x] Received from queue [{queue}]: {message}");
+        await Task.CompletedTask;
+
+    };
+    await channel.BasicConsumeAsync(queue: queue,
+                            autoAck: true,
+                            consumer: consumer);
+
+}
---
+++ FanoutExchange/Producer/Producer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel= await connection.CreateChannelAsync();
+
+const string message = "8ibna w 8ab elebda3";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange:"amq.fanout",
+                                routingKey: string.Empty,
+                                body: body);
+
+Console.WriteLine($" [x] Sent { message}");
