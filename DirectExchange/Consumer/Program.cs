97038a95956a4eca1bcc0c9cb2e2babdbd3bf57e
3moJunioR <m.wa7ba@gmail.com>
2026-06-06 00:32:04 +0300

    DirectExchange

---
+++ DirectExchange/Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+Console.WriteLine($"Waiting fo message");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);
+
+    // write ur method which will do something here
+
+    Console.WriteLine($"[X] {message} were received successfully");
+};
+await channel.BasicConsumeAsync(queue: "q-notification",
+                          autoAck: true,
+                          consumer: consumer);
---
+++ DirectExchange/Producer/Producer/Program.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory =new ConnectionFactory() {HostName="localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+const string message = "Vamooooooooooos";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: "amq.direct"
+                                , routingKey: "k-notification"
+                                , body: body);
+Console.WriteLine($"[X] {message} were sent successfully");
+Console.ReadLine();
