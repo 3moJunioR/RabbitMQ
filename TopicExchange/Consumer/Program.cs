cbee404e5b176e9a802b783df33378777589f980
3moJunioR <m.wa7ba@gmail.com>
2026-06-07 02:23:33 +0300

    TopicExchange

---
+++ TopicExchange/Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using var connection=await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+Console.WriteLine(" [*] Waiting for messages...");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    // write ur method here to process the message
+
+    Console.WriteLine($" [x] Received {message}");
+};
+await channel.BasicConsumeAsync(
+    queue: "q.tpc3",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit");
+Console.ReadLine();
---
+++ TopicExchange/Producer/Producer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using  var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+const string message = "Vamooooooooooos";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: "amq.topic",
+                                routingKey: "h.z",
+                                body: body);
+
+Console.WriteLine($"[X] Sent {message}");
+Console.ReadLine();
