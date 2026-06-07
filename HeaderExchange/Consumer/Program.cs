a82088f2f8ed1696517a8835ff616cab6ebc0e25
3moJunioR <m.wa7ba@gmail.com>
2026-06-08 02:21:23 +0300

    HeaderExchange

---
+++ HeaderExchange/Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using var connection =await factory.CreateConnectionAsync();
+using var channel=await connection.CreateChannelAsync();
+
+Console.WriteLine("Consumer is running and waiting for messages...");
+
+var consumer=new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);
+
+    // write ur method here to process the message
+    Console.WriteLine($"Received message: {message}");
+};
+await channel.BasicConsumeAsync(
+                                queue: "q.header2",
+                                autoAck: true,
+                                consumer: consumer);
+Console.ReadLine();
---
+++ HeaderExchange/Producer/Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() {HostName="localhost" };
+using var connection =await factory.CreateConnectionAsync();
+using var channel =await connection.CreateChannelAsync();
+
+var properties = new BasicProperties();
+properties.Persistent = true;
+
+Dictionary<string, object?> dictionaryheaders = new Dictionary<string, object?>();
+dictionaryheaders.Add("name1", "info");
+
+properties.Headers=dictionaryheaders;
+
+const string message = "Vamoooooooooos";
+var body=Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(
+                                exchange: "amq.headers",
+                                routingKey: string.Empty,
+                                mandatory: false,
+                                basicProperties: properties,
+                                body: body);
+Console.WriteLine($"message [{message}] were sent successfully");
+Console.ReadLine();
